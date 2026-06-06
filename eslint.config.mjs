b7430f7b23369fb015845fa8f22508f591372e4d
morber <4497491+morber11@users.noreply.github.com>
2026-06-06 12:11:24 +0100

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import astroPlugin from 'eslint-plugin-astro';
+import astroParser from 'astro-eslint-parser';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{ts,mts}'],
+        languageOptions: {
+            parser: tsParser,
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+        },
+        rules: {
+            ...tseslint.configs.recommended.rules,
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+            ],
+        },
+    },
+    ...astroPlugin.configs.recommended,
+    {
+        files: ['**/*.astro'],
+        languageOptions: {
+            parser: astroParser,
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                extraFileExtensions: ['.astro'],
+            },
+        },
+    },
+    jsxA11yPlugin.flatConfigs.recommended,
+    {
+        ignores: ['dist/**', '.astro/**', 'node_modules/**'],
+    },
+];
